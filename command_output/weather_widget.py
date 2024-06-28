db8b45cb9283698e417a1baa7d8092de113557a1
jMujunen <jMujunen@github.com>
2024-06-28 13:44:28 -0700

    Revert fsutils migration. It was performed incorrectly

--- command_output/weather_widget.py
+++
@@ -1,4 +0,0 @@
-import Weather
-
-WeatherObject = Weather.get_weather()
-print(f'{WeatherObject.description} {WeatherObject.tostring(WeatherObject.temperature)}°C')
--- scripts/bashhelpers/ColorizeOutput/BashWrapper.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-# BashWrapper class for manipulating shell command output
-
-
-
-
