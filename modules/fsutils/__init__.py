114c50ec8f3bf4be6c6c6f80d425fd7e767f07ca
jMujunen <jMujunen@github.com>
2024-06-28 18:12:08 -0700

    Refactor file structure from fsutils graduation ------------------------------------------------
    - Removed non-exsistant (local) files from remote

--- modules/fsutils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .GenericFile import File
-from .ImageFile import Img
-from .VideoFile import Video
-from .LogFile import Log
-from .ScriptFile import Exe
-from .DirNode import Dir
-
-all = ["GenericFile", "ImageFile", "VideoFile", "LogFile", "ScriptFile", "DirNode"]
