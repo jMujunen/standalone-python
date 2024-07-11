ec69f1e161cbf276830e8eb739f072690a6483e2
jMujunen <jMujunen@github.com>
2024-07-11 12:33:38 -0700

    Refactor sync_obs.py for better organization and performance
    - Added import statements for new modules (Color, ProgressBar)
    - Replaced hardcoded paths with constants in FOOLDERS dictionary
    - Implemented compression functionality for video files
    - Removed unnecessary ctypes imports
    - Updated main function to iterate through directories and compress videos
    - Enhanced error handling by removing corrupt files upon successful compression
    - Improved readability with added comments and formatting
    - Added a ProgressBar to display progress during the sync process

--- scripts/sync_obs.py
+++ scripts/sync_obs.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python3
 """This script mounts the windows ssd, compresses the OBS clips by outputting to local storage"""
 
-import ctypes
-import ctypes.util
 import os
-from sh import mount
 
+from size import Converter
 from fsutils import Video, Dir
+from Color import cprint, fg, style
+from ProgressBar import ProgressBar
 
-mount()
+FOLDERS = {
+    "PLAYERUNKNOWN'S BATTLEGROUNDS": "PUBG",
+}
+OUTPUT_PATH = "/mnt/ssd/OBS/Joona"
+
+
+def main():
+    path = Dir("/mnt/win_ssd/Users/Joona/Videos/NVIDIA/")
+    size_before = 0
+    size_after = 0
+    p = ProgressBar(len(path.videos))
+    for directory in path.dirs:
+        if isinstance(directory, Dir):
+            if directory.is_empty:
+                continue
+            output_dir = os.path.join(OUTPUT_PATH, directory.basename)
+            if directory.basename in FOLDERS:
+                output_dir = os.path.join("/mnt/ssd/OBS/Joona", FOLDERS[directory.basename])
+
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir, exist_ok=True)
+            for vid in directory.videos:
+                p.increment()
+                size_before += vid.size
+                vid.compress(output_dir)
+                new = Video(os.path.join(output_dir, vid.basename))
+                size_after += new.size
+                if not new.is_corrupt:
+                    os.remove(vid.path)
+    cprint(f"Space saved: {Converter(size_before - size_after)}")
+
+
+if __name__ == "__main__":
+    main()
