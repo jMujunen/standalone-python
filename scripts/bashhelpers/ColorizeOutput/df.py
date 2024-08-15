3240251210555c5b0c6014595ef8a992769fc783
jMujunen <jMujunen@github.com>
2024-08-15 14:30:32 -0700

    Lint

--- scripts/bashhelpers/ColorizeOutput/df.py
+++ scripts/bashhelpers/ColorizeOutput/df.py
@@ -7,16 +7,18 @@ from Color import fg, style
 df = Styler("df -h")
 df.remove_by_row("credentials")
 df.remove_by_row("efivars")
-df.colorized_command_output([
-    df.body_style(r"^([^\s]+.*)\n", style.bold),
-    df.body_style(r"(/dev/[ns][vd]\w+)", fg.cyan),
-    df.body_style(r"(tmpfs)", fg.yellow),
-    df.body_style(r"([8-9]\d%)", fg.red),
-    df.body_style(r"[6-7]\d%", fg.orange),
-    df.body_style(r"tmpfs.*(\s\d{2}%)", fg.deeppink),
-    df.body_style(r"tmpfs.*(\s[2-9]%)", fg.yellow),
-    df.body_style(r"(/boot|/home|\s/\s)", fg.green),
-    df.body_style(r"(\d{2}\.\d\.\d\.\d{2,3}:/\w+/?\w+?)", fg.blue),  # 10.0.0.50:/hdd")
-])
+df.colorized_command_output(
+    [
+        df.body_style(r"^([^\s]+.*)\n", style.bold),
+        df.body_style(r"(/dev/[ns][vd]\w+)", fg.cyan),
+        df.body_style(r"(tmpfs)", fg.yellow),
+        df.body_style(r"([8-9]\d%)", fg.red),
+        df.body_style(r"[6-7]\d%", fg.orange),
+        df.body_style(r"tmpfs.*(\s\d{2}%)", fg.deeppink),
+        df.body_style(r"tmpfs.*(\s[2-9]%)", fg.yellow),
+        df.body_style(r"(/boot|/home|\s/\s)", fg.green),
+        df.body_style(r"(\d{2}\.\d\.\d\.\d{2,3}:/\w+/?\w+?)", fg.blue),  # 10.0.0.50:/hdd")
+    ]
+)
 
 print(df.sort())
