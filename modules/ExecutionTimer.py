410b30ba500f69cd571b49baa98beeb48efdd37f
jMujunen <jMujunen@github.com>
2024-09-04 15:07:48 -0700

    # Remove ProgressBar dependency and update class documentation
    - Removed ProgressBar import and usage from ExecutionTimer class.
    - Updated class documentation to reflect changes.

--- modules/ExecutionTimer.py
+++ modules/ExecutionTimer.py
@@ -3,33 +3,21 @@
 
 from time import time
 
-# from ProgressBar import ProgressBar
-
 
 class ExecutionTimer:
-    """Execution Timer: Measure and display execution time for a block of code.
-
-    Methods:
-    --------
-        - `fmttime` (seconds):  Format the execution time to
-        hours, minutes, seconds, and milliseconds.
-    """
+    """Class for timing the execution of a block of code."""
 
     def __init__(self):
-        # progress_bar: ProgressBar | None = None):
         self.start_time = 0
         self.end_time = 0
         self.execution_time = 0
-        # self.progress_bar = progress_bar
 
     def __enter__(self):
+        """Context manager method to start the timer."""
         self.start_time = time()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        # if self.progress_bar is not None:
-        #     self.progress_bar.complete()
-        #     print()
         self.end_time = time()
         self.execution_time = self.end_time - self.start_time
         print(f"\n\033[34mExecution time: {str(self)}\033[0m")
