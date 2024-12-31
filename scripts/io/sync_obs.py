8685ec1af2642f973f222747f4e41a8df7fbd7b8
jMujunen <jMujunen@github.com>
2024-12-30 20:11:59 -0800

    Add temporary directory handling and video compression checks
    - Implement a context manager for temporary directories to handle creation and deletion of directories.
    - Add a progress bar to display the progress of processing videos.
    - Improve error handling by using try-except blocks to handle keyboard interrupts, skipping corrupted or empty videos.

--- scripts/io/sync_obs.py
+++ scripts/io/sync_obs.py
@@ -8,7 +8,9 @@ import momentis.utils
 from Color import cprint
 from fsutils.dir import Dir, obj
 from fsutils.video import Video
+from fsutils.utils.Exceptions import CorruptMediaError
 import os
+from ProgressBar import ProgressBar
 
 FOLDERS = {
     "PLAYERUNKNOWN'S BATTLEGROUNDS": "PUBG",
@@ -32,51 +34,77 @@ KEYWORDS = [
 NULLSIZE = 300
 
 
-def post_process():
-    pass
+class TempDir:
+    """Context manager for temporary directory."""
+
+    def __init__(self, path: str) -> None:
+        self.path = Path(path)
+
+    def __enter__(self):
+        self.path.mkdir(parents=True, exist_ok=True)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for root, dirs, _ in os.walk(self.path, topdown=False):
+            for d in dirs:
+                os.rmdir(os.path.join(root, d))
+        os.rmdir(self.path)
+
+
+class Compress:
+    """Context manager to ensure a file is successfully compressed before removal."""
+
+    def __init__(self, compressed: str | Path) -> None:
+        self.compressed_path = compressed
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.compressed_vid = Video(str(self.compressed_path))
+        if any((self.compressed_vid.is_corrupt, self.compressed_vid.size < 300)):
+            Path(self.compressed_vid.path).unlink()
+            raise CorruptMediaError("Corrupt media detected.", self.compressed_vid.path)
+        print("\033[31mRemoving", self.compressed_vid.path, end="\033[0m\n")
+        Path(self.compressed_vid.path).unlink()
 
 
 def main(input_dir: str, output_dir: str) -> list[Video]:
     """Compress videos specified by input Dir and save them to output Dir."""
+    tmp_path = "./.momentis_tmp"
 
-    # momentis.momentis.main(
-    #     input_path=input_dir,
-    #     keywords=KEYWORDS,
-    #     debug=False,
-    #     output_path=output_dir,
-    # )
-    processed_dir = Dir(os.path.join(input_dir, "opencv-output"))
-    Path(OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
-    if processed_dir.exists():
-        for vid in processed_dir.videos():
-            # Check for videos where no frames got written
-            if vid.size < NULLSIZE:
-                vid.unlink()
-                cprint.debug(f"No frames in {vid.name}. Removed...")
-                continue
+    compressed: list[Video] = []
 
-    return Dir(OUTPUT_PATH).videos
+    with TempDir(tmp_path):
+        # Run momentis on the input directory
+        temp_dir = Dir(tmp_path)
+        momentis.momentis.main(
+            input_path=input_dir,
+            keywords=KEYWORDS,
+            debug=False,
+            output_path=tmp_path,
+        )
+        with ProgressBar(len(list(temp_dir.videos()))) as pb:
+            for vid in temp_dir.videos():
+                try:
+                    # Check for videos where no frames got written
+                    if vid.size < NULLSIZE:
+                        vid.unlink()
+                        cprint.debug(f"No frames in {vid.name}. Removed...")
+                        continue
+                    # Transcode video
+                    video_output_path = Path(output_dir, vid.name)
+                    with Compress(vid.path):
+                        compressed.append(vid.compress(output=video_output_path))
 
-
-def remove_flagged(flagged: list[Video]) -> None:
-    """Remove flagged videos from the input directory."""
-    for video in flagged:
-        if video.name.startswith("cv2_"):
-            original_path = Path(input_dir.path, video.name.removeprefix("cv2_"))
-            if original_path.exists():
-                original_path.unlink()
-                cprint.info(f"Removed original {original_path}")
-            else:
-                cprint.warn(f"Original {original_path} not found.")
+                except KeyboardInterrupt:
+                    break
+                pb.increment()
+    return compressed
 
 
 if __name__ == "__main__":
-    import sys
-
+    # results = main(INPUT_PATH, OUTPUT_PATH)
     results = main(INPUT_PATH, OUTPUT_PATH)
-    if len(sys.argv) > 1 and sys.argv[1] == "--remove" and results:
-        remove_flagged(results)
-    else:
-        print("Results")
-
-        print("\n".join([f"{(vid.name, len(vid))}" for vid in results]))
+    print("Results")
+    print("\n".join([f"{(vid.name, len(vid))}" for vid in results]))
