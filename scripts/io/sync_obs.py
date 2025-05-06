d94670a698063f94c81b31291de8e8580184baa0
jMujunen <jMujunen@github.com>
2025-05-05 18:24:53 -0700

    Add debug flag to cli options

--- scripts/io/sync_obs.py
+++ scripts/io/sync_obs.py
@@ -4,13 +4,14 @@
 from pathlib import Path
 
 import momentis.momentis
-import momentis.utils
 from Color import cprint
 from fsutils.dir import Dir
 from fsutils.video import Video
 from fsutils.utils.Exceptions import CorruptMediaError
 import os
 from ProgressBar import ProgressBar
+import argparse
+
 
 FOLDERS = {
     "PLAYERUNKNOWN'S BATTLEGROUNDS": "PUBG",
@@ -19,22 +20,19 @@ FOLDERS = {
 OUTPUT_PATH = "/mnt/ssd/OBS/Joona/opencv-output"
 INPUT_PATH = "/mnt/win_ssd/Users/Joona/Videos/NVIDIA/PLAYERUNKNOWN'S BATTLEGROUNDS"
 
-KEYWORDS = [
-    "sofunny",
-    "meso",
-    "solunny",
-    "hoff",
-    "ffman",
-    "bartard",
-    "dankniss",
-    "vermeme",
-    "nissev",
-    "you",
-]
 NULLSIZE = 300
+
+KEYWORDS = momentis.utils.parse_keywords()
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", action="store_true")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
+    args = parse_args()
     momentis.momentis.main(
-        input_path=INPUT_PATH, keywords=KEYWORDS, debug=False, output_path=OUTPUT_PATH
+        input_path=INPUT_PATH, keywords=KEYWORDS, debug=args.debug, output_path=OUTPUT_PATH
     )
