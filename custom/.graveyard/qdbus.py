d5f004789a9ce611de74f9b42cf996a8335f43c8
acer_laptop <acer_laptop@github.com>
2025-06-30 22:02:38 -0400

    Add more type hints, add cli to specify system bus

--- custom/.graveyard/qdbus.py
+++ custom/.graveyard/qdbus.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""A simple wrapper around qdbus6 for querying the D-Bus."""
+"""A simple wrapper around qdbus for querying the D-Bus."""
 
 import argparse
 import subprocess
+from typing import Generator
 
-from Color import cprint, fg, style
+from Color import cprint, style
 from ThreadPoolHelper import Pool
 
 IGNORED_METHODS = [
@@ -15,20 +16,34 @@ IGNORED_METHODS = [
     "method QString org.freedesktop.DBus.Peer.GetMachineId()",
     "method void org.freedesktop.DBus.Peer.Ping()",
 ]
-from dataclasses import dataclass, field
-from enum import Enum
 
 
 class Bus:
-    def __init__(self, service):
+    service: str
+    num_objects: int
+    num_methods: int
+
+    def __init__(self, service: str, system: bool = False):
         """Initialize a new Bus object for interacting with a specific D-Bus service.
         service (str): The name of the D-Bus service to interact with.
+
+        Args:
+        -----
+          service : str
+            The dbus service to initialize
+
+          system : bool (optional)
+            Do we want to speak with the system bus?
+
         """
+        self._cmd: str = "qdbus"
+        if system:
+            self._cmd = self._cmd = " ".join((self._cmd, "--system"))
         self.service = service
         self.num_objects = len(self.objects())
         self.num_methods = sum([len(self.methods(obj)) for obj in self.objects()])
 
-    def objects(self) -> list:
+    def objects(self) -> list[str]:
         """Return a list of available services on the bus.
 
         Example:
@@ -40,11 +55,13 @@ class Bus:
         """
         return [
             service.strip()
-            for service in subprocess.getoutput(f"qdbus6 {self.service}").split("\n")
+            for service in subprocess.getoutput(f"{self._cmd} {self.service}").split(
+                "\n"
+            )
             if service != ""
         ]
 
-    def methods(self, obj: str, verbose=False) -> list:
+    def methods(self, obj: str, verbose: bool = False) -> list[str]:
         """Return a list of available methods on the given D-Bus object.
 
         Example:
@@ -60,7 +77,7 @@ class Bus:
         methods = [
             method.strip()
             for method in subprocess.run(
-                ["qdbus6", self.service, obj],
+                [*self._cmd.split(" "), self.service, obj],
                 capture_output=True,
                 text=True,
                 check=False,
@@ -71,7 +88,7 @@ class Bus:
             return [method.split(".")[-1].split("(")[0] for method in methods]
         return methods
 
-    def all_methods(self, verbose=False) -> dict[str, list[str]]:
+    def all_methods(self, verbose: bool = False) -> dict[str, list[str]]:
         """Generate a sequence of tuples containing object and method pairs."""
         return {obj: list(self.methods(obj, verbose)) for obj in self.objects()}
 
@@ -80,9 +97,9 @@ class Bus:
         for method in self.methods(obj, verbose=True):
             if method.split(" ")[0] == "property":
                 try:
-                    method_type, _, return_type, method = method.split(" ")
+                    _, __, ___, method = method.split(" ")
                     method_value = subprocess.run(
-                        ["qdbus6", self.service, obj, method],
+                        [*self._cmd.split(" "), self.service, obj, method],
                         capture_output=True,
                         text=True,
                         check=False,
@@ -99,11 +116,13 @@ class Bus:
         return Bus(bus).all_methods()
 
 
-def services():
+def services() -> Generator[str, None, None]:
     """Yield all available D-Bus services."""
     yield from (
         bus.strip()
-        for bus in subprocess.run("qdbus6", shell=True, capture_output=True, text=True, check=False)
+        for bus in subprocess.run(
+            "qdbus", shell=True, capture_output=True, text=True, check=False
+        )
         .stdout.strip()
         .split("\n")
         if "org" in bus and "systemd1" not in bus and "InputDevice" not in bus
@@ -114,20 +133,20 @@ def muliprocessing(bus: Bus, verbose: bool):
     return bus.all_methods(verbose), bus.service
 
 
-def main(scope: list[str], verbose: bool) -> None:
+def main(scope: list[str], verbose: bool, system: bool = False) -> None:
     # Get a list of all available D-Bus objects on this each service
     pool = Pool()
     print(len(scope))
     if len(scope) <= 1:
-        busses = [Bus(service) for service in services()]
+        busses = [Bus(service, system=system) for service in services()]
     else:
-        busses = [Bus(service) for service in scope]
+        busses = [Bus(service, system=system) for service in scope]
     for result in pool.execute(muliprocessing, busses, verbose, progress_bar=False):
         if result:
             mapping, service = result
             cprint(f"\n{service}", style.bold)
             print("--------------------------")
-            for obj, methods in mapping:
+            for obj, methods in mapping.items():
                 if methods:
                     cprint(obj, style.bold, style.underline)
                     for method in methods:
@@ -135,13 +154,23 @@ def main(scope: list[str], verbose: bool) -> None:
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="List all available D-Bus objects and methods")
+    parser = argparse.ArgumentParser(
+        description="List all available D-Bus objects and methods"
+    )
     parser.add_argument(
         "SERVICES",
         nargs="*",
         help="""Service name to list. If no service is specified, then all services will be listed.""",
         default=[""],
     )
+
+    parser.add_argument(
+        "--system",
+        action="store_true",
+        help="Use the system bus",
+        default=False,
+    )
+
     parser.add_argument(
         "-v",
         "--verbose",
@@ -154,7 +183,9 @@ def parse_args() -> argparse.Namespace:
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args.SERVICES, args.verbose)
+    import sys
+
+    sys.exit(main(args.SERVICES, args.verbose, args.system))
 
 
 """
